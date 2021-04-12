1cd58d40cd05f671a9aacccc9ea2e72ad5fab5ae
Michael Vitollo <ace51689@gmail.com>
2021-04-12 16:23:35 -0400

    Added page buttons to our entertainment list so users can dynamically browse through multiple pages of movies and tv.

--- src/fetch requests/tmdbRequests.js
+++ src/fetch requests/tmdbRequests.js
@@ -1,10 +1,10 @@
 const base_URL = "https://api.themoviedb.org/3/"
 const APIKey = "?api_key=6788151fd970b8ef2326538cd7e0ad87"
 const enUS = "&language=en-US"
-const page = "&page=1"
+const pageParam = "&page="
 
-export const getEntertainment = (medium, sortType) => {
-  return fetch(base_URL + medium + "/" + sortType + APIKey + enUS + page, {
+export const getEntertainment = (medium, sortType, page) => {
+  return fetch(base_URL + medium + "/" + sortType + APIKey + enUS + pageParam + page, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   }).then((res) => res.json())
