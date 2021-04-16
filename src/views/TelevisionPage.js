37b4f4f1290c45694cdc517070c73542c8a6af73
Michael Vitollo <ace51689@gmail.com>
2021-04-16 17:32:35 -0400

    Fixed a bug that would crash the page if the selected entertainment did not have any watch providers returned in the fetch.

--- src/views/TelevisionPage.js
+++ src/views/TelevisionPage.js
@@ -5,14 +5,36 @@ const TelevisionPage = (props) => {
   const [television, setTelevision] = useState({});
   const [crew, setCrew] = useState([]);
   const [cast, setCast] = useState([]);
-  const [watchProviders, setWatchProviders] = useState({})
+  const [watchProviders, setWatchProviders] = useState({
+    streaming: undefined,
+    rent: undefined,
+    buy: undefined,
+    none: undefined
+  })
   const baseUrl = "https://image.tmdb.org/t/p/w500";
 
   useEffect(() => {
     getTelevision(props.match.params.tvId).then((res) => setTelevision(res));
+
     getTelevisionProviders(props.match.params.tvId).then((res) => {
-      setWatchProviders(res.results.US)
-    })
+      if (Object.keys(res.results).length !== 0) {
+        setWatchProviders({
+          stream: res.results.US.flatrate,
+          rent: res.results.US.rent,
+          buy: res.results.US.buy,
+          none: undefined
+        })
+      }
+      else {
+        setWatchProviders({
+          stream: undefined,
+          rent: undefined,
+          buy: undefined,
+          none: null
+        })
+      }
+    });
+
     getTelevisionCredits(props.match.params.tvId).then((res) => {
       setCrew(res.crew)
       setCast(res.cast)
@@ -26,27 +48,29 @@ const TelevisionPage = (props) => {
       <img src={television.poster_path && baseUrl + television.poster_path} alt={`This is the official poseter for ${television.name} `} />
       <p>{television.overview}</p>
 
-      <h3>Watch Providers:</h3>
+      {<h3>Watch Providers:</h3>}
       <ul className="watch-providers">
-        <h4>Streaming:</h4>
+        {watchProviders.stream && <h4>Streaming:</h4>}
         {
-          watchProviders.flatrate && watchProviders.flatrate.map((provider) => {
+          watchProviders.stream && watchProviders.stream.map((provider) => {
             return <li key={provider.provider_id}>{provider.provider_name}</li>
-          }) 
+          })
         }
-        <h4>Rent:</h4>
+        {watchProviders.rent && <h4>Rent:</h4>}
         {
           watchProviders.rent && watchProviders.rent.map((provider) => {
             return <li key={provider.provider_id}>{provider.provider_name}</li>
-          }) 
+          })
         }
-        <h4>Buy:</h4>
+        {watchProviders.buy && <h4>Buy:</h4>}
         {
           watchProviders.buy && watchProviders.buy.map((provider) => {
             return <li key={provider.provider_id}>{provider.provider_name}</li>
-          }) 
+          })
         }
+        {watchProviders.none === null && <div>There are no currently known watch providers. Please try again at a later date.</div>}
       </ul>
+
 
       <ul>
         <h3>Executive Producers:</h3>
