9a09b8bc5078a2486060186dbdab22148df47973
Michael Vitollo <ace51689@gmail.com>
2021-04-19 14:41:12 -0400

    Added Cards to TelevisionPage view.

--- src/views/TelevisionPage.js
+++ src/views/TelevisionPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getTelevision, getTelevisionCredits, getTelevisionProviders } from "../fetch requests/tmdbRequests";
+import { Card } from 'react-bootstrap'
 
 const TelevisionPage = (props) => {
   const [television, setTelevision] = useState({});
@@ -43,61 +44,89 @@ const TelevisionPage = (props) => {
 
   return (
     <>
-      <h1>{television.name}</h1>
-      <h3>{television.tagline}</h3>
-      <img src={television.poster_path && baseUrl + television.poster_path} alt={`This is the official poseter for ${television.name} `} />
-      <p>{television.overview}</p>
-
-      {<h3>Watch Providers:</h3>}
-      <ul className="watch-providers">
-        {watchProviders.stream && <h4>Streaming:</h4>}
-        {
-          watchProviders.stream && watchProviders.stream.map((provider) => {
-            return <li key={provider.provider_id}>{provider.provider_name}</li>
-          })
-        }
-        {watchProviders.rent && <h4>Rent:</h4>}
-        {
-          watchProviders.rent && watchProviders.rent.map((provider) => {
-            return <li key={provider.provider_id}>{provider.provider_name}</li>
-          })
-        }
-        {watchProviders.buy && <h4>Buy:</h4>}
-        {
-          watchProviders.buy && watchProviders.buy.map((provider) => {
-            return <li key={provider.provider_id}>{provider.provider_name}</li>
-          })
-        }
-        {watchProviders.none === null && <div>There are no currently known watch providers. Please try again at a later date.</div>}
-      </ul>
-
-
-      <ul>
-        <h3>Executive Producers:</h3>
-        {crew.map((crew) => {
-          if (crew.job === "Executive Producer") {
-            return <li key={crew.id}>{crew.name}</li>;
-          }
-          return false
-        })}
-        <h3>Director:</h3>
-        {crew.map((crew) => {
-          if (crew.job === "Director") {
-            return <h5>{crew.name}</h5>;
-          }
-          return false
-        })}
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <h1>{television.name}</h1>
+          </Card.Title>
+          <Card.Text>
+            <h3>{television.tagline}</h3>
+            <p>{television.overview}</p>
+          </Card.Text>
+          <Card>
+            <Card.Text>
+              {<h3>Watch Providers:</h3>}
+              <ul className="watch-providers">
+                {watchProviders.stream && <h4>Streaming:</h4>}
+                {watchProviders.stream &&
+                  watchProviders.stream.map((provider) => {
+                    return (
+                      <li key={provider.provider_id}>
+                        {provider.provider_name}
+                      </li>
+                    );
+                  })}
+                {watchProviders.rent && <h4>Rent:</h4>}
+                {watchProviders.rent &&
+                  watchProviders.rent.map((provider) => {
+                    return (
+                      <li key={provider.provider_id}>
+                        {provider.provider_name}
+                      </li>
+                    );
+                  })}
+                {watchProviders.buy && <h4>Buy:</h4>}
+                {watchProviders.buy &&
+                  watchProviders.buy.map((provider) => {
+                    return (
+                      <li key={provider.provider_id}>
+                        {provider.provider_name}
+                      </li>
+                    );
+                  })}
+                {watchProviders.none === null && (
+                  <div>
+                    There are no currently known watch providers. Please try
+                    again at a later date.
+                  </div>
+                )}
+              </ul>
+            </Card.Text>
+          </Card>
+          <Card>
+            <Card.Text>
+              <ul>
+                {crew.some((member) => member.job === "Executive Producer") && <h3>Executive Producers:</h3>}
+                {crew.map((crew) => {
+                  if (crew.job === "Executive Producer") {
+                    return <li key={crew.id}>{crew.name}</li>;
+                  }
+                  return false;
+                })}
+              </ul>
+            </Card.Text>
+          </Card>
+        </Card.Body>
+        <img
+          src={television.poster_path && baseUrl + television.poster_path}
+          alt={`This is the theatrical poster for ${television.name}`}
+        />
+      </Card>
+      <Card>
         <h3>Cast:</h3>
         {cast.map((cast) => {
           return (
             <div key={cast.id}>
-              <img src={baseUrl + cast.profile_path} alt={`This is ${cast.name}`} />
+              <img
+                src={baseUrl + cast.profile_path}
+                alt={`This is ${cast.name}`}
+              />
               <p>{cast.name}</p>
               <p>{cast.character}</p>
             </div>
           );
         })}
-      </ul>
+      </Card>
     </>
   );
 };
