5abf938c5615cd6976d16a1bff2233cdb082aa3c
Timothy <tim.chandler91@gmail.com>
2021-04-21 11:45:10 -0400

    HomePage

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom"
 
 const Home = (props) => {
   return (
-    <div>
-      This would be your home page, currently under construction.
+    <div> 
+      <h1>This would be your home page, currently under construction.</h1>
       <br/>
       Link to: <Link to="/browse">Browse Entertainmnet</Link>
       <br />
       Link to: <Link to="/search">Search Entertainment</Link>
+
+      <div>
+        <h2>
+          Continue Watching?
+        </h2>
+        <h4> Movies/shows that have been watched will go here</h4>
+      </div>
     </div>
+
   )
 }
+ 
 
 export default Home
