4acec63a526b14dd3b4d14140a6562520fad0c88
Timothy <tim.chandler91@gmail.com>
2021-04-23 11:23:53 -0400

    updated

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,17 +5,30 @@ const Home = (props) => {
   return (
     <> 
     <Navigation />
-      <h1>This would be your home page, currently under construction.</h1>
+      <h1>Welcome Back! Lets get started</h1>
     
       <div>
-        <h2>
-          Continue Watching?
-        </h2>
-        <h4> Movies/shows that have been watched will go here</h4>
-      </div>
+        
+        <h2>Your Favorites</h2>
+        
+        </div>
+        <div> 
+          
+          <h2>Movie Ratings</h2>
+          
+          
+        
+        </div>
+        <div>
+          
+          <h2>Reviews</h2>
+        
+        </div>
+      
     </>
 
   )
 }
+
 
 export default Home
